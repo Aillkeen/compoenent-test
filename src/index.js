38706372cb535c06caba37c7e13650bf0e64c013
helljho <helisson.nascimento@ccc.ufcg.edu.br>
2019-03-07 16:57:22 -0300

    melhorias

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import SBPowerSearch from "./lib/SBPowerSearch/SBPowerSearch";
+import SearchType from "./lib/SBPowerSearch/lib/SearchType";
+import SelecStoreInterface from "./lib/SBPowerSearch/lib/SelectStoreInterface";
 
-export { SBPowerSearch };
+export { SBPowerSearch, SearchType, SelecStoreInterface };
