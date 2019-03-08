4a53fa5509f32a558e395f3bbccf2ceba05979cb
helljho <helisson.nascimento@ccc.ufcg.edu.br>
2019-03-08 09:28:19 -0300

    Ajustes no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,93 @@
+const webpack = require("webpack");
+const CleanObsoleteChunks = require("webpack-clean-obsolete-chunks");
+
 var path = require("path");
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2" // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            plugins: ["transform-decorators-legacy"],
-            presets: ["es2015", "stage-0", "react"]
+const ENV = "production";
+const isProd = true;
+const VERSION = JSON.stringify(require("./package.json").version).replace(
+  /["']/g,
+  ""
+);
+const publicPath = path.join(__dirname, "./package.json");
+const shouldUseRelativeAssetPaths = publicPath === "./";
+
+// ExtractTextPlugin expects the build output to be flat.
+// (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
+// However, our output is structured with css, js and media folders.
+// To have this structure working with relative paths, we have to use custom options.
+// const extractTextPluginOptions = shouldUseRelativeAssetPaths
+//   ? // Making sure that the publicPath goes back to to build folder.
+//     { publicPath: Array(cssFilename.split('/').length).join('../') }
+//   : {};
+
+const plugins = [
+  new CleanObsoleteChunks(),
+  new webpack.HotModuleReplacementPlugin()
+];
+
+if (isProd) {
+  plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+      quiet: true
+    })
+  );
+  plugins.push(
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    })
+  );
+}
+
+module.exports = env => {
+  console.log(
+    `|******************* NODE_ENV: ${ENV.toUpperCase()} #### VERSION: ${VERSION} ***********************************|`
+  );
+
+  return {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+      filename: "index.js",
+      path: path.join(__dirname, "build/")
+      // publicPath: '/synbase',
+    },
+    devtool: "inline-source-map",
+    resolve: {
+      alias: {
+        react: path.resolve("./node_modules/react")
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$|\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react"]
+            }
+          }
+        }
+      ]
+    },
+    plugins: plugins,
+    optimization: {
+      minimize: isProd,
+      runtimeChunk: true,
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendor",
+            chunks: "all"
           }
         }
       }
-    ]
-  },
-  externals: {
-    react: "commonjs react" // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-  }
+    }
+  };
 };
