e7c780d62323b255ef5c9380a0b624d2220aa087
Aillkeen <aillkeenoliveira@gmail.com>
2019-03-11 09:45:39 -0300

    Ajuste

--- src/SBPowerSearch/SBPowerSearch.js
+++ src/SBPowerSearch/SBPowerSearch.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Select, Form, Row, Col, DatePicker, Icon, Button } from "antd";
-//import { observer } from "mobx-react";
 import moment from "moment";
 
 import SearchObject from "./lib/SearchObject";
@@ -34,7 +33,7 @@ const buttonsColProps = {
   xl: { span: 3 },
   xxl: { span: 2 }
 };
-//@observer
+
 class SBPowerSearch extends React.Component {
   selectProps;
 
--- src/SBPowerSearch/lib/PowerSearchStore.js
+++ src/SBPowerSearch/lib/PowerSearchStore.js
@@ -1,13 +1,11 @@
-import { observable, action, computed } from 'mobx';
-
 class PowerSearchStore {
-  @observable
+
   filterParameters = [];
 
-  @observable
+
   currentFilter = [];
 
-  @observable
+
   valueList = [];
 
   columns = [];
@@ -16,19 +14,19 @@ class PowerSearchStore {
     this.columns = columns;
   }
 
-  @action
+
   removeFilter(value) {
     this.currentFilter.replace(this.currentFilter.filter(val => val.text !== value));
     this.filterParameters.replace(this.filterParameters.filter(val => val.toString() !== value));
   }
 
   //<currentFilter operators>
-  @action
+
   setCurrentFilter(arrayValue) {
     this.currentFilter.replace(arrayValue);
   }
 
-  @action
+
   pushCurrentFilter(value) {
     this.currentFilter.push(value);
   }
@@ -36,12 +34,12 @@ class PowerSearchStore {
   //</currentFilter operators>
 
   //<valueList operators>
-  @action
+
   setValueList(arrayValue) {
     this.valueList.replace(arrayValue);
   }
 
-  @action
+
   pushValueList(value) {
     this.valueList.push(value);
   }
@@ -49,19 +47,18 @@ class PowerSearchStore {
   //</valueList operators>
 
   //<filterParameters operators>
-  @action
+
   setFilterParameters(arrayValue) {
     this.filterParameters.replace(arrayValue);
   }
 
-  @action
+
   pushFilterParameters(value) {
     this.filterParameters.push(value);
   }
 
   //</filterParameters operators>
 
-  @computed
   get concatArrays() {
     if (this.filterParameters.length > 0) {
       const result = this.filterParameters.map(item => {
