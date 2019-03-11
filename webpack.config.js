617381015f3149e38c9802dde3c220e5f4f4b172
Aillkeen <aillkeenoliveira@gmail.com>
2019-03-11 10:33:20 -0300

    Alteracao no package e no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
 var path = require('path');
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, '/output/'),
-    filename: 'output.js',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        include: path.resolve(__dirname, '/src/'),
-        use: {
+module.exports = () =>  {
+  return {
+    entry: {
+      index: './src/index.js'
+    },
+    output: {
+      path: path.join(__dirname, 'distribution'),
+      filename: '[name].js',
+      publicPath: '/distribution/',
+      library: 'helena',
+      libraryTarget: 'umd',
+    },
+    externals: {
+      react: {
+        root: 'React',
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs: 'react-dom',
+        commonjs2: 'react-dom',
+        amd: 'react-dom',
+      },
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$|\.jsx$/,
+          exclude: /node_modules/,
           loader: 'babel-loader',
-          options: {
-            presets: ["env","es2015", "stage-0", "react"]
-          }
-        }
-      }      
-    ]
-  },
-  externals: {
-    'react': 'commonjs react'
+          query: {
+            plugins: ['transform-decorators-legacy'],
+            presets: ['env', 'es2015', 'stage-0', 'react']},
+        },
+      ],
+    },
   }
 };
